690805b7f8fe3c3c44a7887b1b23db68cc77d591
Amith V <amithv0705@gmail.com>
2025-11-04 22:03:04 +0530

    Setup project structure and logs

---
+++ logger.py
@@ -0,0 +1,35 @@
+import logging
+import os
+from datetime import datetime
+
+def setup_logger(name:str="SmartSumm"):
+    """
+    This file is used to create logs. Anytime you run the program a log file 
+    is generated inside logs/ based on date and time the log file name is created.
+    """
+    log_dir="logs"
+
+    os.makedirs(log_dir,exist_ok=True)
+
+    timestamp=datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    log_filename=os.path.join(log_dir,f"{timestamp}.log")
+
+    #Log format
+    log_format="%(asctime)s | %(levelname)s | %(filename)s:%(lineno)d | %(message)s"
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format=log_format,
+        handlers=[
+            logging.FileHandler(log_filename,encoding="utf-8"),
+            logging.StreamHandler() # To print onto console asw
+        ]
+    )
+
+    logger=logging.getLogger(name)
+    logger.info(f"Logger initalized. Writing logs to:{log_filename}")
+
+    return logger
+
+if __name__=="__main__":
+    setup_logger("Trial")
